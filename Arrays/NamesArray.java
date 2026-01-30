9fdde38aa06c022e860c3104a33b5f7def935e3c
SWASTIK SINGH <neetusingh231001@gmail.com>
2026-01-30 09:57:09 +0530

    Arraya

---
+++ Arrays/NamesArray.java
@@ -0,0 +1,11 @@
+public class NamesArray {
+    public static void main(String[] args) {
+
+        String[] names = {"Varun", "Swastik", "Vivek"};
+
+        for(int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
+    }
+}
